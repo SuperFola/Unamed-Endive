678e942f1137de2d5b527141d2e70f4758a1748f
Loodoor <folaefolc@outlook.fr>
2016-08-20 15:26:10 +0200

    add the assets

---
+++ assets/sons/test_compress.py
@@ -0,0 +1,11 @@
+﻿import pygame, glob, pickle
+
+pygame.init()
+
+e = pygame.display.set_mode((480, 300))
+for file in glob.glob("*.*"):
+    if '.ogg' in file:
+        s = pygame.mixer.Sound(file)
+        pickle.Pickler(open(file + ".datas", "wb")).dump(s.get_raw())
+
+pygame.quit()
