41fefb7c47fa1fd7ae4337d9485ef2eb9a2808f3
Loodoor <folaefolc@outlook.fr>
2017-02-10 22:24:06 +0100

    updating dex and script to get stats about it

--- py-tools/dexstats.py
+++ py-tools/dexstats.py
@@ -8,6 +8,7 @@ def main():
     
     keys = 'type', 'stade'
     stats = {k: {} for k in keys}
+    stats['correct path'] = 0
     
     with open("dex.json") as dex:
         formatted = eval(dex.read())
@@ -18,9 +19,17 @@ def main():
                 stats[k][v[k]] = 1
             else:
                 stats[k][v[k]] += 1
+        if os.path.exists("../creatures/" + v['file']):
+            stats['correct path'] += 1
+    
+    tot = sum(stats['type'].values())
     
     for k, v in stats.items():
-        print(k, "\t* " + "\n\t* ".join(str(_k) + ": " + str(_v) for _k, _v in v.items()), sep='\n')
+        if isinstance(v, dict):
+            print(k, "\t*" + "\n\t*".join("{k:3}: {v:3} - {p:6.2f}%".format(k=_k, v=_v, p=_v / tot * 100) for _k, _v in v.items()), sep='\n')
+        else:
+            print(k + ":", "{v:3} - {p:6.2f}%".format(v=v, p=v / tot * 100))
+    print("Count", "\t " + str(tot), sep="\n")
 
 
 if __name__ == '__main__':
